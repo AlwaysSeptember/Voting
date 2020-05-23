a94ff03b97df4988800e3412b26fc767fefddfef
Tiffany <tiffany.k.taylor@gmail.com>
2020-05-23 13:46:09 -0500

    move convertDataToMotion function

--- src/functions.php
+++ src/functions.php
@@ -6,6 +6,10 @@ declare(strict_types = 1);
  * This file holds functions that are required by all environments.
  */
 
+use ASVoting\Model\Choice;
+use ASVoting\Model\ProposedMotion;
+use ASVoting\Model\Question;
+
 /**
  * @param array $indexes
  * @return mixed
@@ -815,4 +819,28 @@ function showException(\Exception $exception)
         echo "<br/><br/>";
         $exception = $exception->getPrevious();
     } while ($exception !== null);
+}
+
+function convertDataToMotion($data)
+{
+    $questions = [];
+    foreach($data['questions'] as $question) {
+        $choices = [];
+
+        foreach($question['choices'] as $choice) {
+            $choices[] = new Choice($choice['text']);
+        }
+
+        $questions[] = new Question($question['text'], $question['voting_system'], $choices);
+    }
+
+    $proposedMotion = new ProposedMotion(
+        $data['type'],
+        $data['name'],
+        \DateTimeImmutable::createFromFormat( \DateTime::RFC3339, '2020-07-02T12:00:00Z'),
+        \DateTimeImmutable::createFromFormat( \DateTime::RFC3339, '2020-07-02T12:00:00Z'),
+        $questions
+    );
+
+    return $proposedMotion;
 }
