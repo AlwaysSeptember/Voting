1d1cfb6c3b09ada5ebad265c06987a4ec9f73d61
Tiffany <tiffany.k.taylor@gmail.com>
2020-05-23 13:46:09 -0500

    move convertDataToMotion function

--- src/ASVoting/Repo/ProposedMotionExternalSource/CurlProposedMotionExternalSource.php
+++ src/ASVoting/Repo/ProposedMotionExternalSource/CurlProposedMotionExternalSource.php
@@ -9,30 +9,6 @@ use ASVoting\Model\ProposedMotion;
 use ASVoting\Model\Choice;
 use ASVoting\Model\Question;
 
-function convertDataToMotion($data)
-{
-    $questions = [];
-    foreach($data['questions'] as $question) {
-        $choices = [];
-
-        foreach($question['choices'] as $choice) {
-            $choices[] = new Choice($choice['text']);
-        }
-
-        $questions[] = new Question($question['text'], $question['voting_system'], $choices);
-    }
-
-    $proposedMotion = new ProposedMotion(
-         $data['type'],
-         $data['name'],
-         \DateTimeImmutable::createFromFormat( \DateTime::RFC3339, '2020-07-02T12:00:00Z'),
-         \DateTimeImmutable::createFromFormat( \DateTime::RFC3339, '2020-07-02T12:00:00Z'),
-         $questions
-    );
-
-    return $proposedMotion;
-}
-
 class CurlProposedMotionExternalSource implements ProposedMotionExternalSource
 {
     public function getProposedMotionsFromExternalSource(string $source)
