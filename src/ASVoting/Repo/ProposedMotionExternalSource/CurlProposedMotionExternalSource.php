5dfe4557f127a67757344917c75b6a529eb89ae8
Tiffany <tiffany.k.taylor@gmail.com>
2020-05-23 13:43:04 -0500

    complete curlProposedMotionExternalSource

--- src/ASVoting/Repo/ProposedMotionExternalSource/CurlProposedMotionExternalSource.php
+++ src/ASVoting/Repo/ProposedMotionExternalSource/CurlProposedMotionExternalSource.php
@@ -6,6 +6,32 @@ namespace ASVoting\Repo\ProposedMotionExternalSource;
 
 use ASVoting\Repo\ProposedMotionExternalSource\ProposedMotionExternalSource;
 use ASVoting\Model\ProposedMotion;
+use ASVoting\Model\Choice;
+use ASVoting\Model\Question;
+
+function convertDataToMotion($data)
+{
+    $questions = [];
+    foreach($data['questions'] as $question) {
+        $choices = [];
+
+        foreach($question['choices'] as $choice) {
+            $choices[] = new Choice($choice['text']);
+        }
+
+        $questions[] = new Question($question['text'], $question['voting_system'], $choices);
+    }
+
+    $proposedMotion = new ProposedMotion(
+         $data['type'],
+         $data['name'],
+         \DateTimeImmutable::createFromFormat( \DateTime::RFC3339, '2020-07-02T12:00:00Z'),
+         \DateTimeImmutable::createFromFormat( \DateTime::RFC3339, '2020-07-02T12:00:00Z'),
+         $questions
+    );
+
+    return $proposedMotion;
+}
 
 class CurlProposedMotionExternalSource implements ProposedMotionExternalSource
 {
@@ -13,10 +39,11 @@ class CurlProposedMotionExternalSource implements ProposedMotionExternalSource
     {
         $headers = [ 'User-Agent: AlwaysSeptember' ];
         $files = fetchDataWithHeaders($source, $headers);
+        $motions = [];
         foreach($files as $file) {
-            $motionJson = fetchDataWithHeaders($file['download_url'], $headers);
+            $motionData = fetchDataWithHeaders($file['download_url'], $headers);
+            $motions[] = convertDataToMotion($motionData);
         }
-        var_dump($motionJson);
-        exit(0);
+        return $motions;
     }
 }
