11dc97ae7dbe69e50a2c96350f948e705bc0bcb7
Danack <Danack@basereality.com>
2020-05-29 16:31:33 +0100

    Setting up voting end points.

---
+++ src/ASVoting/ApiController/VoteEndpoint.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace ASVoting\ApiController;
+
+use ASVoting\Model\VoteToDelete;
+use ASVoting\Model\VoteToRecord;
+use SlimAuryn\Response\JsonResponse;
+use VarMap\VarMap;
+use ASVoting\Repo\VoteStorage\VoteStorageRepo;
+use Params\Exception\ValidationException;
+
+class VoteEndpoint
+{
+    public function index()
+    {
+        return new JsonResponse(['status' => 'ok']);
+    }
+
+    // TIFF
+    public function postVote(VarMap $varMap, VoteStorageRepo $voteStorageRepo)
+    {
+        try {
+            $vote = VoteToRecord::createFromVarMap($varMap);
+            $voteStorageRepo->recordVote($vote);
+        }
+        catch (ValidationException $ve) {
+
+        }
+        catch (\Exception $e) {
+            // e.g. duplicated
+        }
+
+        return new JsonResponse(['status' => 'ok']);
+    }
+
+    // TIFF
+    public function deleteVote(VarMap $varMap, VoteStorageRepo $voteStorageRepo)
+    {
+        try {
+            $voteToDelete = VoteToDelete::createFromVarMap($varMap);
+            $voteStorageRepo->deleteVote($voteToDelete);
+        }
+        catch (ValidationException $ve) {
+
+        }
+        catch (\Exception $e) {
+            // e.g. duplicated
+        }
+
+        return new JsonResponse(['status' => 'ok']);
+    }
+}
---
+++ src/ASVoting/Model/VoteToDelete.php
@@ -0,0 +1,108 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace ASVoting\Model;
+
+
+use ASVoting\App;
+use ASVoting\ToArray;
+use Params\ExtractRule\GetArrayOfType;
+use Params\ExtractRule\GetString;
+use Params\ExtractRule\GetDatetime;
+use Params\InputParameter;
+use Params\InputParameterList;
+use Params\ProcessRule\MaxLength;
+use Params\ProcessRule\MinLength;
+
+use Params\Create\CreateFromArray;
+use Params\Create\CreateFromJson;
+use Params\Create\CreateFromVarMap;
+
+class VoteToDelete implements InputParameterList
+{
+    use ToArray;
+
+    use CreateFromArray;
+
+    use CreateFromJson;
+
+    use CreateFromVarMap;
+
+    private string $user_id;
+
+    private string $motion_id;
+
+    private string $question_id;
+
+
+    /**
+     *
+     * @param string $user_id
+     * @param string $motion_id
+     * @param string $question_id
+     * @param string $choice
+     */
+    public function __construct(
+        string $user_id,
+        string $motion_id,
+        string $question_id
+    ) {
+        $this->user_id = $user_id;
+        $this->motion_id = $motion_id;
+        $this->question_id = $question_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserId(): string
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMotionId(): string
+    {
+        return $this->motion_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getQuestionId(): string
+    {
+        return $this->question_id;
+    }
+
+    /**
+     * @return \Params\InputParameter[]
+     */
+    public static function getInputParameterList(): array
+    {
+
+        return [
+            new InputParameter(
+                'user_id',
+                new GetString(),
+                new MinLength(4),
+                new MaxLength(2048)
+            ),
+            new InputParameter(
+                'motion_id',
+                new GetString(),
+                new MinLength(4),
+                new MaxLength(2048)
+            ),
+            new InputParameter(
+                'question_id',
+                new GetString(),
+                new MinLength(4),
+                new MaxLength(256)
+            )
+        ];
+    }
+
+}
